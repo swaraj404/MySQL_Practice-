96b0df2646dca130d2dc53969d5074a5ad3a7cdb
Swaraj Pawar <swarajpawar404@gmail.com>
2025-07-04 16:05:11 +0530

    Add files via upload

---
+++ MySQLMusic/src/dev/lpa/Main.java
@@ -0,0 +1,35 @@
+package dev.lpa;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+public class Main {
+    public final static String CONN_STRING = "jdbc:mysql://localhost:3306/music";
+    public static void main(String[] args) {
+
+        String username = JOptionPane.showInputDialog(
+                null,"Enter DB Username");
+        JPasswordField pf = new JPasswordField();
+        int okCxl = JOptionPane.showConfirmDialog(null,pf,"Enter DB Password",JOptionPane.OK_CANCEL_OPTION);
+        final char[] password = (okCxl == JOptionPane.OK_OPTION) ? pf.getPassword() : null;
+
+        var dataSource = new MysqlDataSource();
+        //dataSource.setURL(CONN_STRING);
+        dataSource.setServerName("swaraj404");
+        dataSource.setPort(3306);
+        dataSource.setDatabaseName("music");
+//        try(Connection connection = DriverManager.getConnection(
+//                CONN_STRING, username,String.valueOf(password))){
+        try(Connection connection = DriverManager.getConnection(CONN_STRING,username,String.valueOf(password)) ){
+            System.out.println("Success!! Connection made to the music database.");
+            Arrays.fill(password,' ');
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
